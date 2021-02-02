5a86de53d7c7c748feb2165255e6082ec6188953
DCZ <daniel.czwalinna@alexanderthamm.com>
2021-02-02 22:21:33 +0100

    finalized click-dummy

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h3>Add Todo</h3>
+        <div class="add">
+            <form @submit='onSubmit'>
+                <input type='text' v-model='title' placeholder='Add Todo...'>
+                <input type='submit' value='submit'>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'AddTodo',
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(e) {
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type='text'] {
+        flex: 10;
+        padding: 10px;
+        border: 1px solid black;
+        outline: 0;
+    }
+
+    input[type='submit'] {
+        flex: 2;
+        background: rgb(7, 92, 92);
+        color: white;
+        border: 1px solid black;
+        cursor: pointer;
+    }
+</style>
